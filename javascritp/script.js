101684b82e6942d3709580b6e7c3129bc00b6857
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-06-01 16:50:35 -0300

    terminando o carrossel de imagens

--- javascritp/script.js
+++ javascritp/script.js
@@ -1,11 +1,59 @@
-// const myObserver = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//     } else {
-//       entry.target.classList.remove("show");
-//     }
-//   });
-// });
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-// const elements = document.querySelectorAll(".rolagem");
+const elements = document.querySelectorAll(".rolagem");
+
+elements.forEach((element) => myObserver.observe(element));
+
+//Carrossel
+let currentIndex = 0;
+let intervalId; // Variável para armazenar o ID do intervalo
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".slide");
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  const offset = -currentIndex * 100;
+  document.querySelector(
+    ".carousel"
+  ).style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+function startCarousel() {
+  intervalId = setInterval(nextSlide, 5000);
+}
+
+startCarousel();
+
+document.querySelector(".prev").addEventListener("click", () => {
+  clearInterval(intervalId);
+  startCarousel();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  clearInterval(intervalId);
+  startCarousel();
+});
